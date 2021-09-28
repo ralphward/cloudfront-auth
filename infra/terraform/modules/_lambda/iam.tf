6c066e7a604dda83ec103054f3c3b9d185e4c28a
Chris Wilson <chris.wilson@iress.com>
2021-09-28 11:00:58 +0100

    set resources to kms keys in the current account

--- infra/terraform/modules/_lambda/iam.tf
+++ infra/terraform/modules/_lambda/iam.tf
@@ -26,7 +26,9 @@ data "aws_iam_policy_document" "execution" {
 
   statement {
     actions   = ["kms:Decrypt","kms:GenerateDataKey"]
-    resources = ["*"]
+    resources = [
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"
+    ]
   }
 }
 
