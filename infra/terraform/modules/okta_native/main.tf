8ec77f7ac1a6861e4784f83ab32f1ff2e20827a4
Ralph Ward <ralph.ward@gmail.com>
2022-07-08 12:35:25 +1000

    update path

--- infra/terraform/modules/okta_native/main.tf
+++ infra/terraform/modules/okta_native/main.tf
@@ -4,7 +4,7 @@ module "auth" {
   release_version               = var.release_version
   name                          = var.name
   tags                          = var.tags
-  package_url                   = "https://github.com/iress/cloudfront-auth/releases/download/${var.release_version}/okta_native.zip"
+  package_url                   = "https://github.com/ralphward/cloudfront-auth/releases/download/${var.release_version}/okta_native.zip"
   key_pair_rotation_period_days = var.key_pair_rotation_period_days
   kms_key_arn                   = var.kms_key_arn
 }
